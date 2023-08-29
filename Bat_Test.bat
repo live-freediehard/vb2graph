4c1a39ccc0a5067e8dc4eb2e6b0897ef8b89ec34
m0#|t <143215369+live-freediehard@users.noreply.github.com>
2023-08-29 19:38:54 -0400

    Add files via upload

---
+++ Bat_Test.bat
@@ -0,0 +1,32 @@
+dot -Tpng Actual_test.dot -o Actual_test.png
+dot -Tpng doloopuntil.dot -o doloopuntil.png
+dot -Tpng doloopwhile.dot -o doloopwhile.png
+dot -Tpng dountil.dot -o dountil.png
+dot -Tpng Dowhile.dot -o Dowhile.png
+dot -Tpng For.dot -o For.png
+dot -Tpng For_Test.dot -o For_Test.png
+dot -Tpng Select-if.dot -o Select-if.png
+dot -Tpng Select.dot -o Select.png
+dot -Tpng Source_Test_1.dot -o Source_Test_1.png
+dot -Tpng Source_Test_2.dot -o Source_Test_2.png
+dot -Tpng Source_Test_3.dot -o Source_Test_3.png
+dot -Tpng Source_Test_4.dot -o Source_Test_4.png
+dot -Tpng Source_Test_5.dot -o Source_Test_5.png
+dot -Tpng Source_Test_6.dot -o Source_Test_6.png
+dot -Tpng Source_Test_6b.dot -o Source_Test_6b.png
+dot -Tpng Test1.dot -o Test1.png
+dot -Tpng Test2.dot -o Test2.png
+dot -Tpng Test3.dot -o Test3.png
+dot -Tpng Newtest1.dot -o Newtest1.png
+dot -Tpng Newtest2.dot -o Newtest2.png
+dot -Tpng AfterForm.dot -o AfterForm.png
+dot -Tpng KeyDown.dot -o KeyDown.png
+
+
+
+
+
+
+
+
+
